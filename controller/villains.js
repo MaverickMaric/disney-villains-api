fcfc850ba187b099af29dfbaba942420c0aa61b4
Maric Brandi <theoneandonlymandarr@gmail.com>
2020-11-22 15:10:18 -0500

    add async await  functionality

--- controller/villains.js
+++ controller/villains.js
@@ -1,27 +1,28 @@
-const villains = require('../villains')
+const models = require('../models')
 
-const getAllVillains = (request, response) => {
+const getAllVillains = async (request, response) => {
+  const villains = await models.villains.findAll()
+
   return response.send(villains)
 }
 
-const getVillainBySlug = (request, response) => {
+const getVillainBySlug = async (request, response) => {
   const { slug } = request.params
 
-  const foundVillain = villains.filter((villain) => villain.slug === slug)
+  const foundVillain = await models.villains.findOne({ where: { slug } })
 
-  return foundVillain.length ? response.send(foundVillain) : response.status(404).send(`${slug} not found`)
+  return foundVillain ? response.send(foundVillain) : response.status(404).send(`${slug} not found`)
 }
 
-const saveNewVillain = (request, response) => {
+
+const saveNewVillain = async (request, response) => {
   const { name, movie, slug } = request.body
 
   if (!name || !movie || !slug) {
     return response.status(400).send('The following fields are required: name, movie, slug')
   }
 
-  const newVillain = { name, movie, slug }
-
-  villains.push(newVillain)
+  const newVillain = await models.villains.create({ name, movie, slug })
 
   return response.status(201).send(newVillain)
 }
--- models/villains.js
+++ models/villains.js
@@ -1,8 +1,9 @@
 const villains = (connection, Sequelize) => {
   return connection.define('villains', {
     id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
-    name: { type: Sequelize.VARCHAR(255) },
-    slug: { type: Sequelize.VARCHAR(255) },
+    name: { type: Sequelize.STRING },
+    movie: { type: Sequelize.STRING },
+    slug: { type: Sequelize.STRING },
   }, { paranoid: true })
 }
 
